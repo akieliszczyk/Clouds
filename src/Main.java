b42cdb957b1c83d2960deecb854a5fe311064aaa
akieliszczyk <kurszewska.anna@gmail.com>
2024-01-21 22:07:55 +0100

    Constructor - exercises

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Cloud cirrus = new Cloud(1,2,3);
+
     }
+
 }
+
+    class Cloud {
+        Cloud(int colour, int size, int height) {
+            this.colour = colour;
+            this.size = size;
+            this.height = height;
+
+        }
+
+        int colour;
+        int size;
+        int height;
+
+    }
