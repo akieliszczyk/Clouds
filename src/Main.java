c68b97f312f9a11f8403d77598b96179515c4950
akieliszczyk <kurszewska.anna@gmail.com>
2024-02-12 10:49:00 +0100

    Remove content

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Cloud cirrus = new Cloud(1,2,3);
-
     }
-
 }
 
-    class Cloud {
-        Cloud(int colour, int size, int height) {
-            this.colour = colour;
-            this.size = size;
-            this.height = height;
 
-        }
 
-        int colour;
-        int size;
-        int height;
-
-    }
