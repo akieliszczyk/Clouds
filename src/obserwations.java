d173d3df17469f9b26e94218f12c8f9aa07f5174
akieliszczyk <kurszewska.anna@gmail.com>
2024-02-25 14:27:48 +0100

    F1/2: Generate Cloud Color Data

--- src/obserwations.java
+++ src/obserwations.java
@@ -4,12 +4,21 @@ public class obserwations {
 
     Random rand = new Random();
     private String obserwationTab[][] = new String[2][10];
+    private String cloudColors[] = {"W", "G", "DG", "NB"};
     void generateCoverData() {
         for (int i = 0; i<10; i++) {
             int cover = rand.nextInt(9);
             String textCover = Integer.toString(cover);
             System.out.println(textCover);
             obserwationTab[1][i] = textCover;
+        }
+    }
+
+    void genrateCloudColorData() {
+        for (int i = 0; i < 10; i++) {
+            int color = rand.nextInt(4);
+            obserwationTab[1][i] = cloudColors[color];
+            System.out.println(obserwationTab[1][i]);
         }
     }
 }
