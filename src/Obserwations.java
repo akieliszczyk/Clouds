2643700dcab96e23ec2ac736c211d1ae54dda965
akieliszczyk <kurszewska.anna@gmail.com>
2024-02-27 13:21:49 +0100

    I2: Add more parameters to obserwation table

--- src/Obserwations.java
+++ src/Obserwations.java
@@ -3,18 +3,22 @@ import java.util.Random;
 public class Obserwations {
 
     private Random rand = new Random();
-    private String obserwationTab[][] = new String[2][100];
+    private String obserwationTab[][] = new String[4][100];
     private String cloudColors[] = {"W", "G", "DG", "NB"};
+
+    private String textCover;
+    private String textHeight;
 
     void generateRandomObserwationData(int row) {
         generateCoverData(row);
         generateCloudColorData(row);
+        generateCloudHeighData(row);
     }
 
     private void generateCoverData(int row) {
         for (int i = 0; i<row; i++) {
             int cover = rand.nextInt(9);
-            String textCover = Integer.toString(cover);
+            textCover = Integer.toString(cover);
             obserwationTab[0][i] = textCover;
         }
     }
@@ -24,11 +28,16 @@ public class Obserwations {
             obserwationTab[1][i] = cloudColors[color];
         }
     }
-
+    private void generateCloudHeighData(int row) {
+        for(int i=0; i < row; i++) {
+            int height = rand.nextInt(13);
+            textHeight = Integer.toString(height);
+            obserwationTab[2][i] = textHeight;
+        }
+    }
     void printTable(int row) {
-
         for (int i = 0; i<row; i++) {
-            System.out.println(obserwationTab[0][i] + " " + obserwationTab[1][i]);
+            System.out.println(obserwationTab[0][i] + " " + obserwationTab[1][i] + " " + obserwationTab[2][i] + " " + obserwationTab[3][i]);
 
         }
     }
